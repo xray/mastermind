074545cc299305eb3a389653616527fc5369669a
Jake Lamb <j@kelamb.com>
2018-06-26 10:37:07 -0500

    Fixed naming confusion

--- lib/mastermind_new.rb
+++ lib/mastermind_new.rb
@@ -47,13 +47,13 @@ module Mastermind
     end
 
     class GeneratedList
-        attr_reader :get
+        attr_reader :get_list
     
         def initialize
-            @get = []
+            @get_list = []
             starting_point = { fourth: 1, third: 1, second: 1, first: 1 }
             until starting_point == { fourth: 6, third: 6, second: 6, first: 6 }
-                @get << starting_point.values
+                @get_list << starting_point.values
                 starting_point[:first] += 1
                 starting_point.keys.reverse.map.with_index do |pos, i|
                     if starting_point[pos] > 6
@@ -63,11 +63,11 @@ module Mastermind
                     end
                 end
             end
-            @get << { fourth: 6, third: 6, second: 6, first: 6 }.values
+            @get_list << { fourth: 6, third: 6, second: 6, first: 6 }.values
         end
     
         def random_code
-            @get[rand(1296)]
+            @get_list[rand(1296)]
         end
     end
     
